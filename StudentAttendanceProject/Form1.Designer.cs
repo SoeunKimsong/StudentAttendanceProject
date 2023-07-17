19f63d59c2cffce82bc6a9d3c7283b628742f856
kim_songular <kimsong715@gmail.com>
2023-07-17 08:38:32 +0700

    4 buttons

--- StudentAttendanceProject/Form1.Designer.cs
+++ StudentAttendanceProject/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,18 +45,38 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(527, 343);
+            this.button2.Location = new System.Drawing.Point(581, 325);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(430, 185);
             this.button2.TabIndex = 1;
             this.button2.Text = "Hello";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(655, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(430, 185);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hello";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(66, 325);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(430, 185);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Hello";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1132, 645);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +89,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
