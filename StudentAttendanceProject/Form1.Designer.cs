d9c5ecd8c14513aa3baea9cebd3636baaa34bd1a
Leng <HP23@LAPTOP-LG44BGTQ>
2023-07-17 09:47:57 +0700

    checkbox

--- StudentAttendanceProject/Form1.Designer.cs
+++ StudentAttendanceProject/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(144, 87);
+            this.button1.Location = new System.Drawing.Point(509, 365);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(430, 185);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(581, 325);
+            this.button2.Location = new System.Drawing.Point(132, 130);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(430, 185);
             this.button2.TabIndex = 1;
